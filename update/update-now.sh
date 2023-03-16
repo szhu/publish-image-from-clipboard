062749b90ac09fc37422950716b7083b937731f4
Sean Zhu <interestinglythere@gmail.com>
2023-03-16 14:14:08 -0400

    Add edge detection

--- update/update-now.sh
+++ update/update-now.sh
@@ -11,7 +11,7 @@ verbose() {
 verbose pngpaste update/pasteboard.png
 
 BRIGHTNESS="$(verbose identify -format "%[fx:mean]" update/pasteboard.png)"
-ADJUSTED_BRIGHTNESS="$(echo "(0.4 - $BRIGHTNESS) * 50" | bc)"
+ADJUSTED_BRIGHTNESS="$(echo "(0.5 - $BRIGHTNESS) * 50" | bc)"
 ABS_ADJUSTED_BRIGHTNESS="$(echo "$ADJUSTED_BRIGHTNESS" | sed 's/-//')"
 echo "BRIGHTNESS=$BRIGHTNESS, ADJUSTED_BRIGHTNESS=$ADJUSTED_BRIGHTNESS"
 verbose convert update/pasteboard.png \
@@ -23,14 +23,8 @@ verbose convert update/pasteboard.png \
 verbose rsvg-convert update/index.gen.svg >index.png
 
 # Convert to monochrome image.
-# verbose convert index.png \
-#   -ordered-dither checks \
-#   -brightness-contrast 70x80 \
-#   -remap pattern:gray50 \
-#   -resize 800x480 \
-#   index.png \
-#   ;
 verbose convert index.png \
+  -unsharp 3x30+50-100 \
   -ordered-dither checks \
   -remap pattern:gray50 \
   index.png \
